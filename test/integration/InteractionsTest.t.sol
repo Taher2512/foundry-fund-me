80e5b2224af7df8cca0260b8b55c6280b369ec27
Taher <taherabbkhasamwala@gmail.com>
2024-07-29 10:25:18 +0530

    First Commit

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    DeployFundMe deployFundMe;
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+    }
+
+    // function testUserCanFund() public {
+    //     FundFundMe fundFundMe = new FundFundMe();
+    //     vm.prank(USER);
+    //     vm.deal(USER, 1e18);
+    //     fundFundMe.fundFundMe(address(fundMe));
+
+    //     address funder = fundMe.getFunder(0);
+    //     assertEq(funder, USER);
+
+    //     uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+    //     assertEq(amountFunded, 0.01 ether);
+    // }
+}
